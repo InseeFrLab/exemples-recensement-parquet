b1bb5320f09f9165e255572d4596217a5f3a063a
Antoine Palazzolo <antoine.palazzolo@kes.eleves.polytechnique.fr>
2023-10-25 10:39:15 +0200

    correct dico variables

--- utilitaires/conversion_dico_variables.R
+++ utilitaires/conversion_dico_variables.R
@@ -6,24 +6,28 @@ config <- yaml.load_file("config.yaml")
 path_data <- config$path_data
 
 dico_variables_brut <- read_csv2(
-  paste0(path_data, "/Varmod_LOGEMT_2019.csv")
+  paste0(path_data, "/Dictionnaire variables_logemt_2020.csv")
 ) %>%
-  mutate_all(as.character) %>%
-  select(-LONG_VAR)
+  mutate_all(as.character)
 
-variables_a_grouper <- c("COMMUNE", "ARM", "IRIS", "AEMM", "ANEM", "INEEM",
-                         "INP11M", "INP15M", "INP17M", "INP19M", "INP24M",
-                         "INP3M", "INP5M", "INP60M", "INP65M", "INP75M",
-                         "INPAM", "INPER", "INPER1", "INPER2", "INPOM", "INPSM",
-                         "NBPI", "TRIRIS")
+variables_a_grouper_indiv <- c("AGED", "AGEREV", "ANAI", "INPER", "INPERF", "NBPI")
+
+variables_a_grouper_logement <- c("AEMM", "ANEM", "INEEM",
+                                  "INP11M", "INP15M", "INP17M", "INP19M", "INP24M",
+                                  "INP3M", "INP5M", "INP60M", "INP65M", "INP75M",
+                                  "INPAM", "INPER", "INPER1", "INPER2", "INPOM", "INPSM",
+                                  "NBPI")
+
+# variables_a_grouper <- variables_a_grouper_indiv
+variables_a_grouper <- variables_a_grouper_logement
 
 dico_variables <- dico_variables_brut %>%
   filter(COD_VAR %in% variables_a_grouper) %>%
-  group_by(COD_VAR, LIB_VAR, TYPE_VAR) %>%
+  group_by(COD_VAR, LIB_VAR, TYPE_VAR, LONG_VAR) %>%
   slice(c(1, 2, n())) %>%
   bind_rows(dico_variables_brut %>%
               filter(COD_VAR %in% variables_a_grouper) %>%
-              group_by(COD_VAR, LIB_VAR, TYPE_VAR) %>%
+              group_by(COD_VAR, LIB_VAR, TYPE_VAR, LONG_VAR) %>%
               summarise_all(~ "...")) %>%
   arrange(COD_VAR) %>%
   slice(c(1, 2, 4, 3)) %>%
@@ -32,5 +36,5 @@ dico_variables <- dico_variables_brut %>%
               filter(!COD_VAR %in% variables_a_grouper)) %>%
   arrange(COD_VAR)
 
-write_csv(dico_variables,
-          file = paste0(path_data, "/dictionnaire_variables_2019.csv"))
+write_csv2(dico_variables,
+           file = paste0(path_data, "/dictionnaire_variables_logemt_2020.csv"))
